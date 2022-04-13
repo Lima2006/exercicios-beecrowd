95b223cafaeb15c4b70687952c9feb2dc57ea773
Lima2006 <gabrielchagaslima2006@gmail.com>
2022-04-12 21:58:07 -0300

    1037

--- 1037/index.js
+++ 1037/index.js
@@ -1,3 +1,21 @@
 const filePath = require("path").resolve(__dirname, "./stdin.txt");
 const input = require("fs").readFileSync(filePath, "utf8");
 const lines = input.split("\n");
+
+const firstInputToNumber = (input) => Number(input[0]);
+const calcTheInterval = (value) => {
+  if (value >= 0 && value <= 25) {
+    return "Intervalo [0,25]";
+  } else if (value > 25 && value <= 50) {
+    return "Intervalo (25,50]";
+  } else if (value > 50 && value <= 75) {
+    return "Intervalo (50,75]";
+  } else if (value > 75 && value <= 100) {
+    return "Intervalo (75,100]";
+  } else return "Fora de intervalo";
+};
+const presentAnswer = (answer) => console.log(answer)
+
+const answer = calcTheInterval(firstInputToNumber(lines))
+
+presentAnswer(answer)
