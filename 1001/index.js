2ff49ec59749237270a5887c4fe7924622010f6d
Lima2006 <gabrielchagaslima2006@gmail.com>
2022-06-07 16:48:42 -0300

    refactor: refactor URI 1001

--- 1001/index.js
+++ 1001/index.js
@@ -1,7 +1,22 @@
 const filePath = require("path").resolve(__dirname, "./stdin.txt");
 const input = require("fs").readFileSync(filePath, "utf8");
 const lines = input.split("\n");
+const { evaluateAll } = require("../utils.js");
 
-const [a, b] = lines.map(Number);
-const x = a + b;
-console.log(`X = ${x}`);
+const samples = [
+  { input: "10\n9", output: ["X = 19"] },
+  { input: "-10\n4", output: ["X = -6"] },
+  { input: "15\n-7", output: ["X = 8"] },
+];
+
+const resolution = (lines, console) => {
+  const adaptParams = (lines) => lines.filter(Boolean).map(Number);
+  const sumValues = (values) => values.reduce((acc, value) => acc + value, 0);
+  const formatAnswer = (x) => `X = ${x}`;
+
+  const values = adaptParams(lines);
+  const totalValue = sumValues(values);
+  console.log(formatAnswer(totalValue));
+};
+
+console.table(evaluateAll(resolution, samples));
