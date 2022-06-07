6988f193007bb0df5d6da2d7fbf000e678299f36
Lima2006 <gabrielchagaslima2006@gmail.com>
2022-06-07 20:22:10 -0300

    refactor: URI 1021

--- 1021/index.js
+++ 1021/index.js
@@ -1,56 +1,49 @@
 const filePath = require("path").resolve(__dirname, "./stdin.txt");
 const input = require("fs").readFileSync(filePath, "utf8");
 const lines = input.split("\n");
+const { evaluateAll } = require("../utils.js");
 
-const firstInputToNumber = (input) => Number(input[0]);
-const calcBankNotesAndCoins = (value) => {
-  let remaining = value;
-  const bankNotes = {
-    hundred: 100,
-    fifty: 50,
-    twenty: 20,
-    ten: 10,
-    five: 5,
-    two: 2,
-  };
-  const coins = {
-    hundred: 1,
-    fifty: 0.5,
-    twentyFive: 0.25,
-    ten: 0.1,
-    five: 0.05,
-    one: 0.01,
-  };
-  for (let i in bankNotes) {
-    const bankNoteValue = bankNotes[i];
-    bankNotes[i] = parseInt(remaining / bankNoteValue);
-    remaining = Math.round((remaining % bankNoteValue) * 100) / 100;
-  }
-  for (let i in coins) {
-    const coinValue = coins[i];
-    coins[i] = parseInt(remaining / coinValue);
-    remaining = Math.round((remaining % coinValue) * 100) / 100;
-  }
+const samples = [
+  {
+    input: "576.73\n",
+    output: [
+      "NOTAS:\n5 nota(s) de R$ 100.00\n1 nota(s) de R$ 50.00\n1 nota(s) de R$ 20.00\n0 nota(s) de R$ 10.00\n1 nota(s) de R$ 5.00\n0 nota(s) de R$ 2.00\nMOEDAS:\n1 moeda(s) de R$ 1.00\n1 moeda(s) de R$ 0.50\n0 moeda(s) de R$ 0.25\n2 moeda(s) de R$ 0.10\n0 moeda(s) de R$ 0.05\n3 moeda(s) de R$ 0.01",
+    ],
+  },
+  {
+    input: "4.00\n",
+    output: [
+      "NOTAS:\n0 nota(s) de R$ 100.00\n0 nota(s) de R$ 50.00\n0 nota(s) de R$ 20.00\n0 nota(s) de R$ 10.00\n0 nota(s) de R$ 5.00\n2 nota(s) de R$ 2.00\nMOEDAS:\n0 moeda(s) de R$ 1.00\n0 moeda(s) de R$ 0.50\n0 moeda(s) de R$ 0.25\n0 moeda(s) de R$ 0.10\n0 moeda(s) de R$ 0.05\n0 moeda(s) de R$ 0.01",
+    ],
+  },
+  {
+    input: "91.01\n",
+    output: [
+      "NOTAS:\n0 nota(s) de R$ 100.00\n1 nota(s) de R$ 50.00\n2 nota(s) de R$ 20.00\n0 nota(s) de R$ 10.00\n0 nota(s) de R$ 5.00\n0 nota(s) de R$ 2.00\nMOEDAS:\n1 moeda(s) de R$ 1.00\n0 moeda(s) de R$ 0.50\n0 moeda(s) de R$ 0.25\n0 moeda(s) de R$ 0.10\n0 moeda(s) de R$ 0.05\n1 moeda(s) de R$ 0.01",
+    ],
+  },
+];
 
-  return [ bankNotes, coins ];
+const resolution = (lines, console) => {
+  const bankNotesAndCoins = [
+    100, 50, 20, 10, 5, 2, 1, 0.5, 0.25, 0.1, 0.05, 0.01,
+  ];
+  const inputToNumber = (input) => input.filter(Boolean).map(Number);
+  const calcBankNotes = (input, bankNotes) => {
+    let remaing = input;
+    return bankNotes.reduce((acc, val) => {
+      acc[val] = parseInt(Math.floor(remaing / val));
+      remaing = Math.round((remaing % val) * 100) / 100;
+      return acc;
+    }, {});
+  };
+
+  const formatAnswer = (bankNote) =>
+    `NOTAS:\n${bankNote[100]} nota(s) de R$ 100.00\n${bankNote[50]} nota(s) de R$ 50.00\n${bankNote[20]} nota(s) de R$ 20.00\n${bankNote[10]} nota(s) de R$ 10.00\n${bankNote[5]} nota(s) de R$ 5.00\n${bankNote[2]} nota(s) de R$ 2.00\nMOEDAS:\n${bankNote[1]} moeda(s) de R$ 1.00\n${bankNote[0.5]} moeda(s) de R$ 0.50\n${bankNote[0.25]} moeda(s) de R$ 0.25\n${bankNote[0.1]} moeda(s) de R$ 0.10\n${bankNote[0.05]} moeda(s) de R$ 0.05\n${bankNote[0.01]} moeda(s) de R$ 0.01`;
+
+  const value = inputToNumber(lines)[0];
+  const answer = calcBankNotes(value, bankNotesAndCoins);
+  console.log(formatAnswer(answer));
 };
-const formattedAnswer = ([ bankNotes, coins ]) =>
-  `NOTAS:
-${bankNotes.hundred} nota(s) de R$ 100.00
-${bankNotes.fifty} nota(s) de R$ 50.00
-${bankNotes.twenty} nota(s) de R$ 20.00
-${bankNotes.ten} nota(s) de R$ 10.00
-${bankNotes.five} nota(s) de R$ 5.00
-${bankNotes.two} nota(s) de R$ 2.00
-MOEDAS:
-${coins.hundred} moeda(s) de R$ 1.00
-${coins.fifty} moeda(s) de R$ 0.50
-${coins.twentyFive} moeda(s) de R$ 0.25
-${coins.ten} moeda(s) de R$ 0.10
-${coins.five} moeda(s) de R$ 0.05
-${coins.one} moeda(s) de R$ 0.01`;
-const presentAnswer = (answer) => console.log(formattedAnswer(answer));
 
-const answer = calcBankNotesAndCoins(firstInputToNumber(lines));
-
-presentAnswer(answer);
+console.table(evaluateAll(resolution, samples));
