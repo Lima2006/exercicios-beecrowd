fc91435f3025fa0a0364727b65a64a4ae9ab91d3
Lima2006 <gabrielchagaslima2006@gmail.com>
2022-06-08 12:24:25 -0300

    refacor: URI 1017

--- 1017/index.js
+++ 1017/index.js
@@ -1,17 +1,25 @@
 const filePath = require("path").resolve(__dirname, "./stdin.txt");
 const input = require("fs").readFileSync(filePath, "utf8");
 const lines = input.split("\n");
+const { evaluateAll } = require("../utils");
 
-const calcFuelExpense = (time, velocity, consumption) => {
-  const distance = time * velocity;
-  return distance / consumption;
+const samples = [
+  { input: "10\n85", output: ["70.833"] },
+  { input: "2\n92", output: ["15.333"] },
+  { input: "22\n67", output: ["122.833"] },
+];
+
+const resolution = (lines, console) => {
+  const convertArrayToNumbers = (input) => input.filter(Boolean).map(Number);
+  const calcFuelExpense = (time, velocity, consumption) =>
+    (time * velocity) / consumption;
+  const formatAnswer = (answer) => `${answer.toFixed(3)}`;
+
+  const averageFuelConsumption = 12;
+  const [time, velocity] = convertArrayToNumbers(lines);
+  const answer = calcFuelExpense(time, velocity, averageFuelConsumption);
+
+  console.log(formatAnswer(answer));
 };
-const formattedAnswer = (answer) => `${answer.toFixed(3)}`;
-const inputToNumber = (input) => Number(input);
-const presentAnswer = (answer) => console.log(formattedAnswer(answer));
 
-const averageFuelConsumption = 12;
-const [time, velocity] = [inputToNumber(lines[0]), inputToNumber(lines[1])];
-const answer = calcFuelExpense(time, velocity, averageFuelConsumption);
-
-presentAnswer(answer);
+evaluateAll(resolution, samples);
