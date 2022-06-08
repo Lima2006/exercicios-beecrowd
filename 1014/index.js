f8a0393fc85fbcc74186d5090f479835236bffae
Lima2006 <gabrielchagaslima2006@gmail.com>
2022-06-08 11:42:34 -0300

    refactor: URI 1014

--- 1014/index.js
+++ 1014/index.js
@@ -1,18 +1,23 @@
 const filePath = require("path").resolve(__dirname, "./stdin.txt");
 const input = require("fs").readFileSync(filePath, "utf8");
 const lines = input.split("\n");
+const { evaluateAll } = require("../utils.js");
 
-const inputToNumber = (input) => {
-  for (let i in input) {
-    Number(input[i]);
-  }
-  return input;
+const samples = [
+  { input: "500\n35.0", output: ["14.286 km/l"] },
+  { input: "2254\n124.4", output: ["18.119 km/l"] },
+  { input: "4554\n464.6", output: ["9.802 km/l"] },
+];
+
+const resolution = (lines, console) => {
+  const inputToNumber = (input) => input.filter(Boolean).map(Number);
+  const averageConsumption = (dis, con) => dis / con;
+  const formatAnswer = (answer) => `${answer.toFixed(3)} km/l`;
+
+  const [distance, consumption] = inputToNumber(lines);
+  const answer = averageConsumption(distance, consumption);
+
+  console.log(formatAnswer(answer));
 };
-const averageConsumption = (dis, con) => dis / con;
-const formattedAnswer = (value) => `${value.toFixed(3)} km/l`;
-const presentAnswer = () => console.log(formattedAnswer(answer));
 
-const [distance, consumption] = inputToNumber(lines);
-const answer = averageConsumption(distance, consumption);
-
-presentAnswer();
+evaluateAll(resolution, samples);
