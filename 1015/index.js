9d629c3dc16a04f786e07de389755b644b3ab2b0
Lima2006 <gabrielchagaslima2006@gmail.com>
2022-06-08 11:55:34 -0300

    refactor: URI 1015

--- 1015/index.js
+++ 1015/index.js
@@ -1,25 +1,23 @@
 const filePath = require("path").resolve(__dirname, "./stdin.txt");
 const input = require("fs").readFileSync(filePath, "utf8");
 const lines = input.split("\n");
+const { evaluateAll } = require("../utils.js")
 
-const splitBySpace = (input) => {
-  input = input.split(" ");
-  return input;
-};
-const arrayStringsToNumber = (input) => {
-  for (let n in input) {
-    input[n] = Number(input[n]);
-  }
-  return input;
-};
-const calcDistance = (x1, y1, x2, y2) =>
-  Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-const formattedAnswer = (input) => input.toFixed(4);
+const samples = [
+  {input: "1.0 7.0\n5.0 9.0", output: ["4.4721"]},
+  {input: "-2.5 0.4\n12.1 7.3", output: ["16.1484"]},
+  {input: "2.5 -0.4\n-12.2 7.0", output: ["16.4575"]},
+]
 
-const [x1, y1, x2, y2] = [
-  ...arrayStringsToNumber(splitBySpace(lines[0])),
-  ...arrayStringsToNumber(splitBySpace(lines[1])),
-];
-const answer = calcDistance(x1, y1, x2, y2);
+const resolution = (lines, console) => {
+  const [x1, y1] = lines[0].split(" ").filter(Boolean).map(Number)
+  const [x2, y2] = lines[1].split(" ").filter(Boolean).map(Number)
+  const calcDistance = (x1, y1, x2, y2) =>
+    Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+  const formatAnswer = (input) => input.toFixed(4);
+  const answer = calcDistance(x1, y1, x2, y2);
+  
+  console.log(formatAnswer(answer));
+}
 
-console.log(formattedAnswer(answer));
+evaluateAll(resolution, samples)
