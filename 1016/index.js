0b87e382c0e10446191b91b365ae6749487d3b9c
Lima2006 <gabrielchagaslima2006@gmail.com>
2022-06-08 12:15:41 -0300

    refactor: URI 1016

--- 1016/index.js
+++ 1016/index.js
@@ -1,13 +1,25 @@
 const filePath = require("path").resolve(__dirname, "./stdin.txt");
 const input = require("fs").readFileSync(filePath, "utf8");
 const lines = input.split("\n");
+const { evaluateAll } = require("../utils.js");
 
-const inputToNumber = (input) => Number(input);
-const calcDistance = (input) => input * 2;
-const formattedAnswer = (answer) => `${answer} minutos`;
-const presentAnswer = (input) => console.log(formattedAnswer(input));
+const samples = [
+  { input: "30", output: ["60 minutos"] },
+  { input: "110", output: ["220 minutos"] },
+  { input: "7", output: ["14 minutos"] },
+];
 
-const x = inputToNumber(lines[0]);
-const answer = calcDistance(x);
+const resolution = (lines, console) => {
+  const calcDistance = (velX, velY, distance) =>
+    (60 / (velY - velX)) * distance;
+  const formatAnswer = (answer) => `${answer} minutos`;
 
-presentAnswer(answer);
+  const velocityX = 60;
+  const velocityY = 90;
+  const x = Number(lines[0]);
+  const answer = calcDistance(velocityX, velocityY, x);
+
+  console.log(formatAnswer(answer));
+};
+
+evaluateAll(resolution, samples);
