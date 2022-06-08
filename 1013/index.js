fad5ab17dbe044c0737a5f29c1894dfd13a026b6
Lima2006 <gabrielchagaslima2006@gmail.com>
2022-06-08 11:23:53 -0300

    refactor: URI 1013

--- 1013/index.js
+++ 1013/index.js
@@ -1,29 +1,25 @@
 const filePath = require("path").resolve(__dirname, "./stdin.txt");
 const input = require("fs").readFileSync(filePath, "utf8");
 const lines = input.split("\n");
+const { evaluateAll } = require("../utils.js");
 
-const initialInput = lines[0].split(" ");
-const inputArrayOf3ToNumber = (input) => {
-  for (let i in input) {
-    input[i] = Number(input[i]);
-  }
-  return input;
+const samples = [
+  { input: "7 14 106\n", output: ["106 eh o maior"] },
+  { input: "217 14 6\n", output: ["217 eh o maior"] },
+];
+
+const resolution = (lines, console) => {
+  const convertLineToNumbers = (input) => input.split(" ").map(Number);
+  const higherNumber = (input) => {
+    let sorted = input.slice(0);
+    sorted.sort((a, b) => b - a);
+    return sorted[0];
+  };
+
+  const numbers = convertLineToNumbers(lines[0]);
+  const answer = higherNumber(numbers);
+  const formatAnswer = (answer) => `${answer} eh o maior`;
+  console.log(formatAnswer(answer));
 };
-const higherNumber = (input) => {
-  let higher = 0;
-  let num = inputArrayOf3ToNumber(input);
-  for (let i = 0; i < input.length - 1; i++) {
-    const calcHigher =
-      (num[i] + num[i + 1] + Math.abs(num[i] - num[i + 1])) / 2;
-    if (calcHigher > higher) {
-      higher = calcHigher;
-    }
-  }
-  return higher;
-};
 
-const answer = higherNumber(initialInput);
-const formattedAnswer = `${answer} eh o maior`;
-const presentAnswer = () => console.log(formattedAnswer);
-
-presentAnswer();
+evaluateAll(resolution, samples);
