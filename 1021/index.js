4bdc7aff4454f1da7bbebd3ab9840494453da56b
Lima2006 <gabrielchagaslima2006@gmail.com>
2022-06-08 10:20:49 -0300

    refactor: URI 1021

--- 1021/index.js
+++ 1021/index.js
@@ -25,25 +25,38 @@ const samples = [
 ];
 
 const resolution = (lines, console) => {
-  const bankNotesAndCoins = [
-    100, 50, 20, 10, 5, 2, 1, 0.5, 0.25, 0.1, 0.05, 0.01,
-  ];
+  const bankNotes = [100, 50, 20, 10, 5, 2];
+  const coins = [1, 0.5, 0.25, 0.1, 0.05, 0.01];
+  const bankNotesAndCoins = [...bankNotes, ...coins];
   const inputToNumber = (input) => input.filter(Boolean).map(Number);
-  const calcBankNotes = (input, bankNotes) => {
-    let remaing = input;
+  const calcBankNotesandCoins = (input, bankNotes) => {
+    let remaining = input;
     return bankNotes.reduce((acc, val) => {
-      acc[val] = parseInt(Math.floor(remaing / val));
-      remaing = Math.round((remaing % val) * 100) / 100;
+      acc[val] = Math.floor(remaining / val);
+      remaining = Number((remaining % val).toFixed(2));
       return acc;
     }, {});
   };
-
-  const formatAnswer = (bankNote) =>
-    `NOTAS:\n${bankNote[100]} nota(s) de R$ 100.00\n${bankNote[50]} nota(s) de R$ 50.00\n${bankNote[20]} nota(s) de R$ 20.00\n${bankNote[10]} nota(s) de R$ 10.00\n${bankNote[5]} nota(s) de R$ 5.00\n${bankNote[2]} nota(s) de R$ 2.00\nMOEDAS:\n${bankNote[1]} moeda(s) de R$ 1.00\n${bankNote[0.5]} moeda(s) de R$ 0.50\n${bankNote[0.25]} moeda(s) de R$ 0.25\n${bankNote[0.1]} moeda(s) de R$ 0.10\n${bankNote[0.05]} moeda(s) de R$ 0.05\n${bankNote[0.01]} moeda(s) de R$ 0.01`;
+  const formatBankNotes = (amount, value) =>
+    `${amount} nota(s) de R$ ${value.toFixed(2)}`;
+  const formatCoins = (amount, value) =>
+    `${amount} moeda(s) de R$ ${value.toFixed(2)}`;
 
   const value = inputToNumber(lines)[0];
-  const answer = calcBankNotes(value, bankNotesAndCoins);
-  console.log(formatAnswer(answer));
+  const sortedBankNotesAndCoins = calcBankNotesandCoins(
+    value,
+    bankNotesAndCoins
+  );
+  bankNotesMap = bankNotes
+    .map((bankNote) =>
+      formatBankNotes(sortedBankNotesAndCoins[bankNote], bankNote)
+    )
+    .join("\n");
+  coinsMap = coins
+    .map((coin) => formatCoins(sortedBankNotesAndCoins[coin], coin))
+    .join("\n");
+  const formattedAnswer = `NOTAS:\n${bankNotesMap}\nMOEDAS:\n${coinsMap}`;
+  console.log(formattedAnswer);
 };
 
-console.table(evaluateAll(resolution, samples));
+evaluateAll(resolution, samples)
