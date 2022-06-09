d020cd0cbc65c6006c1fde7eac59bc27ebb205a9
Lima2006 <gabrielchagaslima2006@gmail.com>
2022-06-09 18:07:08 -0300

    refactor: URI 1020

--- 1020/index.js
+++ 1020/index.js
@@ -1,24 +1,35 @@
 const filePath = require("path").resolve(__dirname, "./stdin.txt");
 const input = require("fs").readFileSync(filePath, "utf8");
 const lines = input.split("\n");
+const { evaluateAll } = require("../utils.js");
 
-const firstInputToNumber = (input) => Number(input[0]);
-const calcDate = (days) => {
-  const result = {
-    years: 0,
-    months: 0,
-    days: 0,
+const samples = [
+  { input: "400", output: ["1 ano(s)\n1 mes(es)\n5 dia(s)"] },
+  { input: "800", output: ["2 ano(s)\n2 mes(es)\n10 dia(s)"] },
+  { input: "30", output: ["0 ano(s)\n1 mes(es)\n0 dia(s)"] },
+];
+
+const resolution = (lines, console) => {
+  const calcDate = (days) => {
+    const result = {
+      remaining: days,
+      years: 0,
+      months: 0,
+      days: 0,
+    };
+    result.years = Math.floor(result.remaining / 365);
+    result.remaining = result.remaining % 365;
+    result.months = Math.floor(result.remaining / 30);
+    result.days = result.remaining % 30;
+    return [result.years, result.months, result.days];
   };
-  result.days = (days % 365) % 30;
-  result.months = Math.floor((days % 365) / 30);
-  result.years = Math.floor(days / 365);
-  return [result.years, result.months, result.days];
+  const formatAnswer = (years, months, days) =>
+    `${years} ano(s)\n${months} mes(es)\n${days} dia(s)`;
+
+  const days = Number(lines[0]);
+  const answer = calcDate(days);
+
+  console.log(formatAnswer(...answer));
 };
-const formattedAnswer = (years, months, days) =>
-  `${years} ano(s)\n${months} mes(es)\n${days} dia(s)`;
-const presentAnswer = (answer) => console.log(formattedAnswer(...answer));
 
-const days = firstInputToNumber(lines);
-const answer = calcDate(days);
-
-presentAnswer(answer);
+evaluateAll(resolution, samples);
