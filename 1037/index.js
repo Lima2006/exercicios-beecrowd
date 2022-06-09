697bc9bd4e997759d7e62946dd72ee63b29505ba
Lima2006 <gabrielchagaslima2006@gmail.com>
2022-06-09 19:54:39 -0300

    refactor: URI 1037

--- 1037/index.js
+++ 1037/index.js
@@ -1,21 +1,28 @@
 const filePath = require("path").resolve(__dirname, "./stdin.txt");
 const input = require("fs").readFileSync(filePath, "utf8");
 const lines = input.split("\n");
+const { evaluateAll } = require("../utils.js");
 
-const firstInputToNumber = (input) => Number(input[0]);
-const calcTheInterval = (value) => {
-  if (value >= 0 && value <= 25) {
-    return "Intervalo [0,25]";
-  } else if (value > 25 && value <= 50) {
-    return "Intervalo (25,50]";
-  } else if (value > 50 && value <= 75) {
-    return "Intervalo (50,75]";
-  } else if (value > 75 && value <= 100) {
-    return "Intervalo (75,100]";
-  } else return "Fora de intervalo";
+const samples = [
+  { input: "25.01", output: ["Intervalo (25,50]"] },
+  { input: "25.00", output: ["Intervalo [0,25]"] },
+  { input: "100.00", output: ["Intervalo (75,100]"] },
+  { input: "-25.02", output: ["Fora de intervalo"] },
+];
+
+const resolution = (lines, console) => {
+  const calcTheInterval = (value) => {
+    if (value >= 0 && value <= 25) return "Intervalo [0,25]";
+    if (value > 25 && value <= 50) return "Intervalo (25,50]";
+    if (value > 50 && value <= 75) return "Intervalo (50,75]";
+    if (value > 75 && value <= 100) return "Intervalo (75,100]";
+    return "Fora de intervalo";
+  };
+
+  const primaryValue = Number(lines[0]);
+  const answer = calcTheInterval(primaryValue);
+
+  console.log(answer);
 };
-const presentAnswer = (answer) => console.log(answer)
 
-const answer = calcTheInterval(firstInputToNumber(lines))
-
-presentAnswer(answer)
+evaluateAll(resolution, samples);
