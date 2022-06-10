1ff0776fe86594446d3e84790fad5241350bd6a8
Lima2006 <gabrielchagaslima2006@gmail.com>
2022-06-10 17:16:55 -0300

    refactor: URI 1046

--- 1046/index.js
+++ 1046/index.js
@@ -2,26 +2,26 @@ const filePath = require("path").resolve(__dirname, "./stdin.txt");
 const input = require("fs").readFileSync(filePath, "utf8");
 const lines = input.split("\n");
 
-const splitNumbersBySpace = (input) => {
-  const values = input.split(" ");
-  for (let i in values) {
-    values[i] = Number(values[i]);
-  }
-  return values;
-};
-const gameDuration = ([start, end]) => {
-  if (end - start < 0) {
-    return end + 24 - start;
-  } else if (end - start === 0) {
-    return 24;
-  } else if (end - start > 0) {
+const resolution = (lines, console) => {
+  const splitNumbersBySpace = (input) => input.split(" ").map(Number);
+
+  const gameDuration = ([start, end]) => {
+    const isOtherDay = end - start < 0;
+    if (isOtherDay) {
+      return end + 24 - start;
+    }
+    const isFullDay = end - start === 0;
+    if (isFullDay) {
+      return 24;
+    }
     return end - start;
-  }
+  };
+  const formattedAnswer = (input) => `O JOGO DUROU ${input} HORA(S)`;
+
+  const baseValues = splitNumbersBySpace(lines[0]);
+  const answer = gameDuration(baseValues);
+
+  console.log(formattedAnswer(answer));
 };
-const formattedAnswer = (input) => `O JOGO DUROU ${input} HORA(S)`;
-const presentAnswer = (input) => console.log(formattedAnswer(input));
 
-const baseValues = splitNumbersBySpace(lines[0]);
-const answer = gameDuration(baseValues);
-
-presentAnswer(answer);
+resolution(lines, console);
