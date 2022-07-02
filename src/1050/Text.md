31d25082cd94771689676e3a6093560d9054627d
Lima2006 <gabrielchagaslima2006@gmail.com>
2022-07-01 21:15:37 -0300

    URI 1050 done

---
+++ src/1050/Text.md
@@ -0,0 +1,22 @@
+# DDD
+
+Leia um número inteiro que representa um código de DDD para discagem interurbana. Em seguida, informe à qual cidade o DDD pertence, considerando a tabela abaixo:
+
+![Tabela DDD](https://resources.beecrowd.com.br/gallery/images/problems/UOJ_1050.png)
+
+Se a entrada for qualquer outro DDD que não esteja presente na tabela acima, o programa deverá informar: <br>
+DDD nao cadastrado
+
+## Entrada
+
+A entrada consiste de um único valor inteiro.
+
+## Saída
+
+Imprima o nome da cidade correspondente ao DDD existente na entrada. Imprima DDD nao cadastrado caso não existir DDD correspondente ao número digitado.
+
+---
+
+| Exemplos de Entrada | Exemplos de Saída |
+| :------------------ | :---------------- |
+| 11                  | Sao Paulo         |
---
+++ src/1050/index.js
@@ -0,0 +1,29 @@
+const filePath = require("path").resolve(__dirname, "./stdin.txt");
+const input = require("fs").readFileSync(filePath, "utf8");
+const { evaluateAll, transformTextToSmples } = require("../utils");
+
+const samples = transformTextToSmples(input);
+
+const dddList = {
+  61: "Brasilia",
+  71: "Salvador",
+  11: "Sao Paulo",
+  21: "Rio de Janeiro",
+  32: "Juiz de Fora",
+  19: "Campinas",
+  27: "Vitoria",
+  31: "Belo Horizonte",
+};
+
+const resolution = (lines, console) => {
+  const adaptInput = (input) => Number(input[0]);
+  const findDDD = ({ ddd, dddList }) => dddList[ddd] || "DDD nao cadastrado";
+
+  const dddNumber = adaptInput(lines);
+
+  const answer = findDDD({ ddd: dddNumber, dddList });
+
+  console.log(answer);
+};
+
+evaluateAll(resolution, samples);
