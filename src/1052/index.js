5252efd33e30b7423a85913ad2f2f1e0f36f2d13
Lima2006 <gabrielchagaslima2006@gmail.com>
2022-07-02 23:31:37 -0300

    URI 1052 done

---
+++ src/1052/index.js
@@ -0,0 +1,32 @@
+const filePath = require("path").resolve(__dirname, "./stdin.txt");
+const input = require("fs").readFileSync(filePath, "utf8");
+const { evaluateAll, transformTextToSmples } = require("../utils");
+
+const samples = transformTextToSmples(input);
+
+const monthsList = {
+  1: "January",
+  2: "February",
+  3: "March",
+  4: "April",
+  5: "May",
+  6: "June",
+  7: "July",
+  8: "August",
+  9: "September",
+  10: "October",
+  11: "November",
+  12: "December",
+};
+
+const resolution = (lines, console) => {
+  const adaptInput = (input) => Number(input[0]);
+  const findMonth = ({ monthsList, monthNumber }) => monthsList[monthNumber];
+
+  const monthNumber = adaptInput(lines);
+  const answer = findMonth({ monthNumber, monthsList });
+
+  console.log(answer);
+};
+
+evaluateAll(resolution, samples);
